0ef254ff72ca303d4bdf50f0214750c6c671c71d
TheSharpOwl <13377190+TheSharpOwl@users.noreply.github.com>
2024-08-22 02:22:01 +0400

    Fix the bug for rendering the board + add drawing when mouse is over the squares.

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
+function createBoardSquare() {
+  let rowElem = document.createElement("div");
+  rowElem.style.backgroundColor = "#FFD700";
+  rowElem.style.width = "100%";
+  rowElem.style.height = "100%";
+  rowElem.style.margin = 0;
+  rowElem.style.border = "thin solid #000000"
+  rowElem.onmouseover = function(){this.style.backgroundColor="#4B0082";}
+
+  return rowElem;
+}
+
+function createBoardRow(n) {
+  let rowRef = document.createElement("div");
+  rowRef.className = "board-row";
+  rowRef.style.width = "100%";
+  rowRef.style.height = "100%";
+  rowRef.style.margin = 0;
+  for (let i = 0; i < n; i++) {
+    rowElem = createBoardSquare();
+    rowRef.appendChild(rowElem);
+  }
+
+  return rowRef;
+}
+
 const generateBoard = (n) => {
   // inside of this div we will make many rows and align them like a column
   const rowsDiv = document.getElementById("board-container");
   rowsDiv.className = "board-column";
-  //   rowsDiv.style.width = "100%";
-  //   rowsDiv.style.height = "100%";
   let rowRefs = [];
 
   for (let j = 0; j < n; j++) {
-    let rowRef = document.createElement("div");
-    rowRef.className = "board-row";
-    rowRef.style.width = 20 + "%";
-    rowRef.style.height = 20 + "%";
-    rowRef.style.margin = 0;
-
+    let rowRef = createBoardRow(n);
     rowRefs.push(rowRef);
-    for (let i = 0; i < n; i++) {
-      let rowElem = document.createElement("div");
-      rowElem.style.backgroundColor = (i + j) % 2 == 0 ? "green" : "blue";
-      rowElem.style.width = 20 + "%";
-      rowElem.style.height = 20 + "%";
-      rowElem.style.margin = 0;
-      // maybe add padding here for the size
-      rowRef.appendChild(rowElem);
-    }
     rowsDiv.appendChild(rowRef);
   }
   const boardContainerElem = document.getElementById("board-container");
   boardContainerElem.appendChild(rowsDiv);
 };
 
-// generateBoard(5);
+generateBoard(16);
